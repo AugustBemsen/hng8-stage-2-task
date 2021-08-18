6f269c4659d95ca114e99f2312d4411efff134ec
AugustBemsen <danireptor7@gmail.com>
2021-08-18 15:24:04 +0100

    log name

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import GlobalStyle from "./assets/GlobalStyles";
 import Body from "./components/Body/Body";
 
 function App() {
+  const myName = "Daniel";
+  console.log(myName);
   return (
     <>
       <GlobalStyle />
